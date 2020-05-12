f8fd23edf10177a738a77defb756db91354236f6
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-05-12 12:51:39 +0200

    Get bestsellers lists data from API

--- src/components/BestsellersLists.vue
+++ src/components/BestsellersLists.vue
@@ -1,14 +1,38 @@
 <template>
   <div>
     <h3>Best Sellers lists</h3>
-    
+
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'BestsellersLists'
+  name: 'BestsellersLists',
+  data() {
+    return {
+      bestSellers: []
+    }
+  },
+  methods: {
+    getLists() {
+      console.log('Work')
+      this.$http
+        .get(
+          `https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=iC5ZgYPG8g6NpG9VdPw0iI4XIC37fTGs`
+        )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.bestSellers = data.results;
+          console.log(this.bestSellers)
+        });
+    }
+  },
+  mounted: function() {
+    this.getLists();
+  }
 }
 </script>
 

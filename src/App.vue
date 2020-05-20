27cdd6c9fb9fa1ead582650d4735489e2457f508
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-05-20 15:58:58 +0200

    Add routing to the project

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="app">
     <Navigation />
-    <HomePage />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue'
 import Navigation from './components/Navigation.vue'
 
 export default {
   name: 'App',
   components: {
-    HomePage,
     Navigation
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,13 +3,22 @@ import 'bootstrap-css-only/css/bootstrap.min.css'
 import 'mdbvue/lib/css/mdb.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+import { routes } from './routes';
 
 import App from './App.vue'
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+});
 
 Vue.config.productionTip = false
 
 new Vue({
+    router,
     render: h => h(App),
 }).$mount('#app')

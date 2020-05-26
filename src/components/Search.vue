a83f221d211d88e5ad7d2c42da4024ad37aefd7c
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-05-26 11:57:40 +0200

    Add search form in Search component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>Search</div>
+  <div>
+    <form class="container">
+      <mdb-row>
+        <mdb-col class="col-10">
+            <mdb-input type="text" placeholder="Search" aria-label="Search"/>
+        </mdb-col>
+        <mdb-col class="col-2 d-flex align-items-center">
+          <mdb-btn size="sm" color="indigo">Indigo</mdb-btn>
+        </mdb-col>
+      </mdb-row>
+    </form>
+  </div>
 </template>
 
 <script>
-  // import { mdbFooter, mdbContainer } from 'mdbvue';
+  import { mdbInput, mdbRow, mdbCol, mdbBtn } from 'mdbvue';
+
   export default {
-    name: 'Search'
+    name: 'Search',
+    components: {
+      mdbInput,
+      mdbRow, 
+      mdbCol,
+      mdbBtn
+    }
   }
 </script>

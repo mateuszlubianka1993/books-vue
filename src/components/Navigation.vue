b0229a4891776cac3767788888afcad3e9e6d18d
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-05-26 10:32:47 +0200

    Edit navigation links

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,28 +1,19 @@
 <template>
   <mdb-navbar expand="large" dark color="indigo">
     <mdb-navbar-brand href="#">
-      Navbar
+      Books App
     </mdb-navbar-brand>
     <mdb-navbar-toggler>
       <mdb-navbar-nav right>
         <router-link to="/" ><mdb-nav-item active>Home</mdb-nav-item></router-link>
-        <mdb-nav-item href="#">Link</mdb-nav-item>
-        <mdb-nav-item class="disabled" href="#">Disabled</mdb-nav-item>
-        <mdb-dropdown tag="li" class="nav-item">
-          <mdb-dropdown-toggle tag="a" navLink color="indigo" slot="toggle" waves-fixed>Dropdown</mdb-dropdown-toggle>
-          <mdb-dropdown-menu>
-            <mdb-dropdown-item>Action</mdb-dropdown-item>
-            <mdb-dropdown-item>Another action</mdb-dropdown-item>
-            <mdb-dropdown-item>Something else here</mdb-dropdown-item>
-          </mdb-dropdown-menu>
-        </mdb-dropdown>
+        <mdb-nav-item href="#">Search</mdb-nav-item>
       </mdb-navbar-nav>
     </mdb-navbar-toggler>
   </mdb-navbar>
 </template>
 
 <script>
-import { mdbDropdown, mdbDropdownToggle, mdbDropdownMenu, mdbDropdownItem, mdbNavbar, mdbNavbarBrand, mdbNavbarToggler, mdbNavbarNav, mdbNavItem } from 'mdbvue';
+import { mdbNavbar, mdbNavbarBrand, mdbNavbarToggler, mdbNavbarNav, mdbNavItem } from 'mdbvue';
 
 export default {
   name: 'Navigation',
@@ -31,11 +22,7 @@ export default {
       mdbNavbarBrand,
       mdbNavbarToggler,
       mdbNavbarNav,
-      mdbNavItem,
-      mdbDropdown,
-      mdbDropdownToggle,
-      mdbDropdownMenu,
-      mdbDropdownItem
+      mdbNavItem
     }
 }
 </script>

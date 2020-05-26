b545dca2c3b0635f97beb2e593737e84c83b17b9
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-05-26 12:20:20 +0200

    Add getBooks method, connect with books API

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <form class="container">
+    <form class="container" @submit.prevent="getBooks">
       <mdb-row>
         <mdb-col class="col-10">
-            <mdb-input type="text" placeholder="Search" aria-label="Search"/>
+            <mdb-input v-model="searched" type="text" placeholder="Search" aria-label="Search"/>
         </mdb-col>
         <mdb-col class="col-2 d-flex align-items-center">
-          <mdb-btn size="sm" color="indigo">Indigo</mdb-btn>
+          <mdb-btn type="submit" size="sm" color="indigo">Indigo</mdb-btn>
         </mdb-col>
       </mdb-row>
     </form>
@@ -23,6 +23,27 @@
       mdbRow, 
       mdbCol,
       mdbBtn
+    },
+    data() {
+      return {
+        searched: '',
+        books: []
+      }
+    },
+    methods: {
+      getBooks() {
+        this.$http
+        .get(
+          `https://www.googleapis.com/books/v1/volumes?q=${this.searched}+intitle:${this.searched}`
+        )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.books = data.items;
+          console.log(this.books)
+        });
+      }
     }
   }
 </script>
